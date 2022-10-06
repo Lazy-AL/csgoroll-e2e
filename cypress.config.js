823fbc5309612294747491dd49a55bcb7235bc7d
Lazy-AL <anri.lazashvilib@gmail.com>
2022-10-07 00:38:09 +0400

    feat: changed base url, added test for bet +1,+10,1/2,x2

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://csgoroll-www-master-h7r4kpopga-uc.a.run.app/dice',
+    viewportWidth: 1400,
+    viewportHeight: 1000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/csgoroll-E2E.cy.ts
@@ -0,0 +1,34 @@
+describe('csgoroll-E2E-test', () => {
+  it('visit the website', () => {
+    // cy.request('')
+    cy.visit('/', {
+      auth: {username: 'ancient', password: 'things',}
+    });
+
+  });
+
+  it("Bet +1 / +10 / 1/2 / X2 buttons work as expected", () => {
+    let num = 0
+    cy.get('span[data-cy="value"]').as("profit")
+    cy.get('button[data-test="clear"]').click()
+    //plus 1
+    cy.get('button[data-test="plus-1"]').click()
+    num += 1
+    cy.get('@profit').should("contain", `${num.toFixed(2)}`, {timeout: 1000})
+    //plus 10
+    cy.get('button[data-test="plus-10"]').click()
+    num += 10
+    cy.get('@profit').should("contain", `${num.toFixed(2)}`, {timeout: 1000})
+    //devide by 2
+    cy.get('button[data-test="1-div-2"]').click()
+    num /= 2
+    cy.get('@profit').should("contain", `${num.toFixed(2)}`, {timeout: 1000})
+    //multiply by 2
+    cy.get('button[data-test="x2"]').click()
+    num *= 2
+    cy.get('@profit').should("contain", `${num.toFixed(2)}`, {timeout: 1000})
+  })
+
+})
+
+
